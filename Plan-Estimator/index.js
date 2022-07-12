804f3dd42d532e68fc2fc3f553334bba74046265
dvargasanchondo <asahel950@gmail.com>
2022-07-12 05:16:24 -0500

    Update index.js

--- Plan-Estimator/index.js
+++ Plan-Estimator/index.js
@@ -8,41 +8,55 @@ function calc() {
   let yValuesP1 = new Array(3);
   let yValuesP2 = new Array(3);
   let yValuesP3 = new Array(3);
+  let p1 = new Array();
+  let p2 = new Array();
+  let p3 = new Array();
   let tisch = new Array();
+  let tisch2 = new Array();
   temp = Math.round(hourlyGoal);
   halfHour = Math.round(hourlyGoal)/2;
   for (let i = 0; i < 3; i++) {
     
-    
     yValuesP1[i] = (temp);
+    p1.push(temp/2);
     tisch.push(temp);
+    tisch2.push(temp/2);
     temp = temp + hourlyGoal;
   }
   
+  p1.unshift(0);
   yValuesP1.unshift(0);
   tisch.push(temp-hourlyGoal);
+  tisch2.push(temp/2);
   
   for (let i = 0; i < 4; i++) {
     
     
     yValuesP2[i] = (temp);
+    p2.push(temp/2);
     tisch.push(temp);
+    tisch2.push(temp/2);
     temp = temp + hourlyGoal;
   }
+  
   tisch.push(temp-hourlyGoal);
+  p2.unshift(null,null,null,null,p1[3]);
   yValuesP2.unshift(null,null,null,null,yValuesP1[3]);
   yValuesP2[8] = yValuesP2[8] - halfHour;
 
   for (let i = 0; i < 4; i++) {
+    p3.push(temp/2);
     yValuesP3[i] = (temp);
     tisch.push(temp);
+    tisch2.push(temp/2);
     temp = temp + halfHour;
     }
-    
-
+    document.getElementById("test").innerHTML = tisch2.toString();
+    p3.unshift(null,null,null,null,null,null,null,null,null,p2[8]);
   yValuesP3.unshift(null,null,null,null,null,null,null,null,null,yValuesP2[8]);
   yValuesP3[10] = yValuesP3[10] - halfHour;
   tisch.unshift(0);
+  tisch2.unshift(0);
   
   //document.getElementById("test").innerHTML = tisch.toString();
 
@@ -93,5 +107,56 @@ for (i = 0; i < times.length; i++){
 
   $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
 }
+
+
+
+
+if (this.chart1 && this.chart1 != null) {
+  this.chart1.destroy()
+}
+this.chart1 = new Chart("pdRate", {
+type: "line",
+data: {
+  labels: xValues,
+  datasets: [{
+    label: 'P1', 
+    borderColor: "red",
+    fill: false,
+    data: p1,
+    lineTension:0
+  }, { 
+    label: 'P2',
+    data: p2,
+    borderColor: "green",
+    fill: false
+  }, { 
+    label: 'P3',
+    data: p3,
+    borderColor: "blue",
+    fill: false
+  }]
+},
+options: {
+  tooltips:{
+    mode: 'index',
+    intersect: false
+  },
+  hover:{
+    mode: 'index',
+    intersect: false
+  },
+  legend: {display: true},
+  scales: {
+      yAxes:[{ticks: {min: 0, max: temp + 1000}}]
+  }
+}
+});
+
+for (i = 0; i < times.length; i++){
+  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch2[i]) +"</> </td>");
+
+  $('#tab_logic2').append('<tr id="addr2'+(i+1)+'"></tr>');
+}
+
 
 }
