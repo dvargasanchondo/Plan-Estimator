c9bbae0f9e72725c10f6fb029077f7ba7055321a
dvargasanchondo <asahel950@gmail.com>
2022-11-15 01:47:07 -0600

    Update index.js

--- Plan-Estimator/index.js
+++ Plan-Estimator/index.js
@@ -18,6 +18,7 @@ function calcDay() {
   
   var vendor = document.getElementById('vGoal').value;
   var pallet = document.getElementById('pGoal').value;
+  var trans = document.getElementById('tGoal').value;
   
  //set array for the x values in the graph 
   var xValues = ["07:00", "19:00", "20:00", "21:00", "21:30", "22:30", "23:30", "00:00", "00:30", "01:00", "01:30", "02:30", "03:30", "04:30" ];
@@ -30,6 +31,7 @@ function calcDay() {
 for (var i = h; i < 18; ++i) {
     for (var j = i==h ? (m/1) : 0; j < 60; ++j) { 
         timeArray.push(i%12 + ':' + (j<10?'0' + j*1 : j*1||'00') + ' ' + meridiem[i/12|0] + ' ');
+        
         
     }
 }
@@ -49,56 +51,92 @@ for (var i = h; i < 18; ++i) {
   let pg3 = new Array();
   //these are the arrays for the data tables tisch is the vendor goal and tisch2 is the PID/Dock
   let tisch = new Array();
-  let tisch2 = new Array();
+  let tTisch = new Array();
   //variables to calculate the hourly goal and round the int
   phourlyGoal = Math.round(pallet/9.5);
   phourlyTemp = Math.round(phourlyGoal);
-  hourlyGoal= Math.round(vendor/9.5);
-  temp = Math.round(hourlyGoal);
+  hourlyGoal= (vendor/9.5);
   halfHour = Math.round(hourlyGoal)/2;
 
   // there are three sets of for loops that represent each period.  
   minGoal = hourlyGoal/60;
 
   
-i = 0;
+  i = 0;
    let y23 = new Array();
-   let newMinGoal = new Array();
    temp = 0;
    takeBreak = y23[0] * 30;
-    while(i <= 630 ){
-      if(i > 180 && i < 210 || i > 390 && i < 420){
+    while(i <= 630){
+      
+      if(i > 180 && i < 209 || i > 390 && i < 420){
         y23.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y23.push(Math.floor(minGoal + temp));
+        
       }
 
       else{
-        y23.push(minGoal + temp);
         temp = temp + minGoal;
+        y23.push(temp);
+        
          
       }
-      i= i+1;
-      
-      
+      i = i+1; 
     }
+
+    thourlyGoal = (trans/9.5);
+    tminGoal = thourlyGoal/60;
+    i = 0;
+   let y24 = new Array();
+   tTemp = 0;
+   takeBreak = y24[0] * 30;
+    while(i <= 630){
+      
+      if(i > 180 && i < 209 || i > 390 && i < 420){
+        y24.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y24.push(Math.floor(tminGoal + tTemp));
+        
+      }
+
+      else{
+        tTemp = tTemp + tminGoal;
+        y24.push(tTemp);
+        
+         
+      }
+      i = i+1; 
+    }
+
     newTisch = 0;
     tisch.push(0);
-    for (i = 0; i < times.length; i++){
+    tTisch.push(0);
+    for (i = 0; i < times.length ; i++){
       if( i == 3 || i ==7){
         tisch.push(tisch[i]);
+        tTisch.push(tTisch[i]);
         j=j+30;
+        h=h+30;
       }
-      if(i == 12){
-        j=j-30;
-        tisch.push(Math.round(y23[j]));
+      else if (i == 11){
+        tisch.push(Math.round(y23[630]));
+        tTisch.push(Math.round(y24[630]));
         
       }
       else{
         tisch.push(Math.round(y23[j]));
-      j=j+60;
+        tTisch.push(Math.round(y24[j]));
+        j=j+60;
+        h=h+60;
       }
+    
+      
+    
       
       
-      $('#addr'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>");
+      $('#addr'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (tTisch[i]) +"</> </td>");
     
       $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
       //newTisch = (newTisch + hourlyGoal);
@@ -106,7 +144,7 @@ i = 0;
     }
    
    
-    //document.getElementById("test").innerHTML = timeArray.toString();
+    document.getElementById("test").innerHTML = tTisch.toString();
   if (this.chart && this.chart != null) {
     this.chart.destroy()
   }
@@ -115,12 +153,17 @@ i = 0;
   data: {
     labels: timeArray,
     datasets: [{
-      label: 'Trend', 
+      label: 'Vendor Trend', 
       borderColor: "red",
       fill: true,
       data: y23,
       lineTension:0
-    }, 
+    }, { 
+      label: 'Tranship Trend',
+      data: y24,
+      borderColor: "blue",
+      fill: false
+    }
     ]
   },
   options: {
@@ -186,7 +229,7 @@ options: {
 });
 
 for (i = 0; i < times.length; i++){
-  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch2[i]) +"</> </td>" + "<td>"+ (pg1[i]) +"</> </td>");
+  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (pg1[i]) +"</> </td>");
 
   $('#tab_logic2').append('<tr id="addr2'+(i+1)+'"></tr>');
   
@@ -205,22 +248,24 @@ for (i = 0; i < times.length; i++){
 }
 
 
-function calcNight() {
+function calcDay() {
   
   var vendor = document.getElementById('vGoal').value;
   var pallet = document.getElementById('pGoal').value;
+  var trans = document.getElementById('tGoal').value;
   
  //set array for the x values in the graph 
-  var xValues = ["18:00", "19:00", "20:00", "21:00", "21:30", "22:30", "23:30", "00:00", "00:30", "01:00", "01:30", "02:30", "03:30", "04:30" ];
-  var times = ["18:00", "19:00", "20:00", "21:00", "21:30", "22:30", "23:30", "00:00", "00:30", "01:00", "01:30", "02:30", "03:30", "04:30" ];
+  var xValues = ["07:00", "19:00", "20:00", "21:00", "21:30", "22:30", "23:30", "00:00", "00:30", "01:00", "01:30", "02:30", "03:30", "04:30" ];
+  var times = ["07:00", "08:00", "09:00", "10:00", "10:30", "11:30", "12:30", "01:30", "02:00", "03:00", "04:00", "05:00", "05:30"];
   timeArray = [],
     d = new Date(),
-    h = 18,
+    h = 7,
     m = 00,
-    meridiem = ['PM','AM'];
-for (var i = h; i < 30; ++i) {
+    meridiem = ['AM','PM'];
+for (var i = h; i < 18; ++i) {
     for (var j = i==h ? (m/1) : 0; j < 60; ++j) { 
-        timeArray.push(i%12 + ':' + (j<10?'0' + j*1 : j*1||'00') + ' ' + meridiem[i/24|0] + ' ');
+        timeArray.push(i%12 + ':' + (j<10?'0' + j*1 : j*1||'00') + ' ' + meridiem[i/12|0] + ' ');
+        
         
     }
 }
@@ -240,98 +285,100 @@ for (var i = h; i < 30; ++i) {
   let pg3 = new Array();
   //these are the arrays for the data tables tisch is the vendor goal and tisch2 is the PID/Dock
   let tisch = new Array();
-  let tisch2 = new Array();
+  let tTisch = new Array();
   //variables to calculate the hourly goal and round the int
   phourlyGoal = Math.round(pallet/9.5);
   phourlyTemp = Math.round(phourlyGoal);
-  hourlyGoal= Math.round(vendor/9.5);
-  temp = Math.round(hourlyGoal);
+  hourlyGoal= (vendor/9.5);
   halfHour = Math.round(hourlyGoal)/2;
 
   // there are three sets of for loops that represent each period.  
-
-  for (let i = 0; i < 3; i++) {
-    
-    yValuesP1[i] = (temp);
-    p1.push(temp/2);
-    tisch.push(temp);
-    tisch2.push(temp/2);
-    temp = temp + hourlyGoal;
-    pg1.push(phourlyTemp);
-    phourlyTemp = (phourlyTemp + phourlyGoal);
-  }
-  
-  p1.unshift(0);
-  yValuesP1.unshift(0);
-  tisch.push(temp-hourlyGoal);
-  tisch2.push(temp/2 - halfHour);
-  pg1.push(phourlyTemp - phourlyGoal);
-  
-  
-  
-  for (let i = 0; i < 4; i++) {
-    
-    
-    yValuesP2[i] = (temp);
-    p2.push(temp/2);
-    tisch.push(temp);
-    tisch2.push(temp/2);
-    temp = temp + hourlyGoal;
-    pg1.push(phourlyTemp);
-    phourlyTemp = (phourlyTemp + phourlyGoal);
-  }
-  
-  tisch.push(temp-hourlyGoal);
-  tisch2.push(temp/2 - halfHour);
-  p2.unshift(null,null,null,null,p1[3]);
-  yValuesP2.unshift(null,null,null,null,yValuesP1[3]);
-  pg1[7] = phourlyTemp - phourlyGoal - phourlyGoal/2;
-  yValuesP2[8] = yValuesP2[8] - halfHour;
-  //pg1.push(pg1[8]);
-  phourlyTemp = pg1[7];
-  //document.getElementById("test").innerHTML = phourlyTemp.toString();
-
-
-  for (let i = 0; i < 4; i++) {
-    p3.push(temp/2);
-    yValuesP3[i] = (temp);
-    tisch.push(temp);
-    tisch2.push(temp/2);
-    temp = temp + halfHour;
-    pg1.push(phourlyTemp);
-    phourlyTemp = (phourlyTemp + phourlyGoal);
-    }
-
-    p3.unshift(null,null,null,null,null,null,null,null,null,p2[8]);
-  yValuesP3.unshift(null,null,null,null,null,null,null,null,null,yValuesP2[8]);
-  yValuesP3[10] = yValuesP3[10] - halfHour;
-  tisch.unshift(0);
-  tisch2.unshift(0);
-  pg1.unshift(0);
-  pg1.push("Clean Up");
-  
   minGoal = hourlyGoal/60;
-i = 0;
+
+  
+  i = 0;
    let y23 = new Array();
-   let newMinGoal = new Array();
    temp = 0;
    takeBreak = y23[0] * 30;
-    while(i <= 630 ){
-      if(i > 180 && i < 210 || i > 390 && i < 420){
+    while(i <= 630){
+      
+      if(i > 180 && i < 209 || i > 390 && i < 420){
         y23.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y23.push(Math.floor(minGoal + temp));
+        
       }
 
       else{
-        y23.push(minGoal + temp);
         temp = temp + minGoal;
+        y23.push(temp);
+        
          
       }
-      i= i+1;
+      i = i+1; 
+    }
+
+    thourlyGoal = (trans/9.5);
+    tminGoal = thourlyGoal/60;
+    i = 0;
+   let y24 = new Array();
+   tTemp = 0;
+   takeBreak = y24[0] * 30;
+    while(i <= 630){
       
+      if(i > 180 && i < 209 || i > 390 && i < 420){
+        y24.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y24.push(Math.floor(tminGoal + tTemp));
+        
+      }
+
+      else{
+        tTemp = tTemp + tminGoal;
+        y24.push(tTemp);
+        
+         
+      }
+      i = i+1; 
+    }
+
+    newTisch = 0;
+    tisch.push(0);
+    tTisch.push(0);
+    for (i = 0; i < times.length ; i++){
+      if( i == 3 || i ==7){
+        tisch.push(tisch[i]);
+        tTisch.push(tTisch[i]);
+        j=j+30;
+        h=h+30;
+      }
+      else if (i == 11){
+        tisch.push(Math.round(y23[630]));
+        tTisch.push(Math.round(y24[630]));
+        
+      }
+      else{
+        tisch.push(Math.round(y23[j]));
+        tTisch.push(Math.round(y24[j]));
+        j=j+60;
+        h=h+60;
+      }
+    
+      
+    
+      
+      
+      $('#addr'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (tTisch[i]) +"</> </td>");
+    
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      //newTisch = (newTisch + hourlyGoal);
       
     }
    
-  document.getElementById("test").innerHTML = y23.toString();
+   
+    document.getElementById("test").innerHTML = tTisch.toString();
   if (this.chart && this.chart != null) {
     this.chart.destroy()
   }
@@ -340,17 +387,18 @@ i = 0;
   data: {
     labels: timeArray,
     datasets: [{
-      label: 'Trend', 
+      label: 'Vendor Trend', 
+      borderColor: "red",
       fill: true,
       data: y23,
-      lineTension:0,
-      borderColor: 'rgb(255,0,0)',
-      segment: {
-        borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0)'),
-        borderDash: ctx => skipped(ctx, [6, 6]),
-      },
-
-    }]
+      lineTension:0
+    }, { 
+      label: 'Tranship Trend',
+      data: y24,
+      borderColor: "blue",
+      fill: false
+    }
+    ]
   },
   options: {
     tooltips:{
@@ -368,11 +416,7 @@ i = 0;
   }
 });
 
-for (i = 0; i < times.length; i++){
-  $('#addr'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>");
 
-  $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
-}
 
 
 
@@ -419,7 +463,238 @@ options: {
 });
 
 for (i = 0; i < times.length; i++){
-  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch2[i]) +"</> </td>" + "<td>"+ (pg1[i]) +"</> </td>");
+  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (pg1[i]) +"</> </td>");
+
+  $('#tab_logic2').append('<tr id="addr2'+(i+1)+'"></tr>');
+  
+}
+
+//for (i = 0; i < times.length; i++){
+  //$('#addr3'+i).html("<td>"+ (times[i]) +"</td><td>"+ (pg1[i]) +"</> </td>");
+
+ // $('#tab_logic3').append('<tr id="addr3'+(i+1)+'"></tr>');
+//}
+
+
+ 
+
+
+}
+function calcNight() {
+  
+  var vendor = document.getElementById('vGoal').value;
+  var pallet = document.getElementById('pGoal').value;
+  var trans = document.getElementById('tGoal').value;
+  
+ //set array for the x values in the graph 
+  var xValues = ["07:00", "19:00", "20:00", "21:00", "21:30", "22:30", "23:30", "00:00", "00:30", "01:00", "01:30", "02:30", "03:30", "04:30" ];
+  var times = ["06:00", "07:00", "08:00", "09:00", "09:30", "10:30", "11:30", "12:30", "01:00", "02:00", "03:00", "04:00", "04:30"];
+  timeArray = [],
+    d = new Date(),
+    h = 18,
+    m = 00,
+    meridiem = ['PM','AM'];
+for (var i = h; i < 30; ++i) {
+    for (var j = i==h ? (m/1) : 0; j < 60; ++j) { 
+        timeArray.push(i%12 + ':' + (j<10?'0' + j*1 : j*1||'00') + ' ' + meridiem[i/24|0] + ' ');
+        
+    }
+}
+
+  // creates the arrays that are going to be used for the datasets
+  //these are the arrays for the overall vendor goal
+  let yValuesP1 = new Array(3);
+  let yValuesP2 = new Array(3);
+  let yValuesP3 = new Array(3);
+  //these are the arrays for the PID/Dock
+  let p1 = new Array();
+  let p2 = new Array();
+  let p3 = new Array();
+  //these are the arrays for the pallet goal
+  let pg1 =  new Array();
+  let pg2 =  new Array();
+  let pg3 = new Array();
+  //these are the arrays for the data tables tisch is the vendor goal and tisch2 is the PID/Dock
+  let tisch = new Array();
+  let tTisch = new Array();
+  //variables to calculate the hourly goal and round the int
+  phourlyGoal = Math.round(pallet/9.5);
+  phourlyTemp = Math.round(phourlyGoal);
+  hourlyGoal= (vendor/9.5);
+  halfHour = Math.round(hourlyGoal)/2;
+
+  // there are three sets of for loops that represent each period.  
+  minGoal = hourlyGoal/60;
+
+  
+  i = 0;
+   let y23 = new Array();
+   temp = 0;
+   takeBreak = y23[0] * 30;
+    while(i <= 630){
+      
+      if(i > 180 && i < 209 || i > 390 && i < 420){
+        y23.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y23.push(Math.floor(minGoal + temp));
+        
+      }
+
+      else{
+        temp = temp + minGoal;
+        y23.push(temp);
+        
+         
+      }
+      i = i+1; 
+    }
+
+    thourlyGoal = (trans/9.5);
+    tminGoal = thourlyGoal/60;
+    i = 0;
+   let y24 = new Array();
+   tTemp = 0;
+   takeBreak = y24[0] * 30;
+    while(i <= 630){
+      
+      if(i > 180 && i < 209 || i > 390 && i < 420){
+        y24.push("NaN");
+      }
+      else if(i == 180 || i == 210 || i == 390 || i == 420){
+         y24.push(Math.floor(tminGoal + tTemp));
+        
+      }
+
+      else{
+        tTemp = tTemp + tminGoal;
+        y24.push(tTemp);
+        
+         
+      }
+      i = i+1; 
+    }
+
+    newTisch = 0;
+    tisch.push(0);
+    tTisch.push(0);
+    for (i = 0; i < times.length ; i++){
+      if( i == 3 || i ==7){
+        tisch.push(tisch[i]);
+        tTisch.push(tTisch[i]);
+        j=j+30;
+        h=h+30;
+      }
+      else if (i == 11){
+        tisch.push(Math.round(y23[630]));
+        tTisch.push(Math.round(y24[630]));
+        
+      }
+      else{
+        tisch.push(Math.round(y23[j]));
+        tTisch.push(Math.round(y24[j]));
+        j=j+60;
+        h=h+60;
+      }
+    
+      
+    
+      
+      
+      $('#addr'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (tTisch[i]) +"</> </td>");
+    
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      //newTisch = (newTisch + hourlyGoal);
+      
+    }
+   
+   
+    document.getElementById("test").innerHTML = tTisch.toString();
+  if (this.chart && this.chart != null) {
+    this.chart.destroy()
+  }
+ this.chart = new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: timeArray,
+    datasets: [{
+      label: 'Vendor Trend', 
+      borderColor: "red",
+      fill: true,
+      data: y23,
+      lineTension:0
+    }, { 
+      label: 'Tranship Trend',
+      data: y24,
+      borderColor: "blue",
+      fill: false
+    }
+    ]
+  },
+  options: {
+    tooltips:{
+      mode: 'index',
+      intersect: false
+    },
+    hover:{
+      mode: 'index',
+      intersect: false
+    },
+    legend: {display: true},
+    scales: {
+        yAxes:[{ticks: {min: 0, max: temp + 1000}}]
+    }
+  }
+});
+
+
+
+
+
+
+if (this.chart1 && this.chart1 != null) {
+  this.chart1.destroy()
+}
+this.chart1 = new Chart("pdRate", {
+type: "line",
+data: {
+  labels: xValues,
+  datasets: [{
+    label: 'P1', 
+    borderColor: "red",
+    fill: false,
+    data: p1,
+    lineTension:0
+  }, { 
+    label: 'P2',
+    data: p2,
+    borderColor: "green",
+    fill: false
+  }, { 
+    label: 'P3',
+    data: p3,
+    borderColor: "blue",
+    fill: false
+  }]
+},
+options: {
+  tooltips:{
+    mode: 'index',
+    intersect: false
+  },
+  hover:{
+    mode: 'index',
+    intersect: false
+  },
+  legend: {display: true},
+  scales: {
+      yAxes:[{ticks: {min: 0, max: temp + 1000}}]
+  }
+}
+});
+
+for (i = 0; i < times.length; i++){
+  $('#addr2'+i).html("<td>"+ (times[i]) +"</td><td>"+ (tisch[i]) +"</> </td>" + "<td>"+ (pg1[i]) +"</> </td>");
 
   $('#tab_logic2').append('<tr id="addr2'+(i+1)+'"></tr>');
   
@@ -437,19 +712,19 @@ for (i = 0; i < times.length; i++){
 
 }
 
-hourlyGoal = 1500;
-minGoal = hourlyGoal/60;
-i = 0;
-   let y23 = new Array();
-   let newMinGoal = new Array();
-   temp = 0;
-    while(i <= 570 ){
+// hourlyGoal = 1500;
+// minGoal = hourlyGoal/60;
+// i = 0;
+//    let y23 = new Array();
+//    let newMinGoal = new Array();
+//    temp = 0;
+//     while(i <= 570 ){
       
-      y23.push(minGoal + temp);
+//       y23.push(minGoal + temp);
       
       
-      temp = temp + minGoal;
-      i= i+1;
+//       temp = temp + minGoal;
+//       i= i+1;
       
-    }
-     document.getElementById("test21").innerHTML = y23.toString();
+//     }
+//      document.getElementById("test21").innerHTML = y23.toString();
